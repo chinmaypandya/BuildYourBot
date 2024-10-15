97594e83b081e9c09dea647a4a99cc7e7b385709
dhruvluniya11 <155226524+dhruvluniya11@users.noreply.github.com.>
2024-10-15 12:07:43 +0530

    Fix(Chat):Config file

--- chat/utils/config.py
+++ chat/utils/config.py
@@ -3,7 +3,8 @@ from dotenv import load_dotenv
 
 # Load environment variables from the .env file
 load_dotenv()
+api_key = os.getenv("API_KEY")
 
 # Store your OpenAI API key here
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "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")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", api_key)
 
