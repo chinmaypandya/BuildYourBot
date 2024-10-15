8eeb40362493d4e095f344cba9e68e4a362886d9
Chinmay Pandya <chinmaypandya043@gmail.com>
2024-10-15 12:09:16 +0530

    feat(agent): simple agent

--- server/src/buildyourbot/agents/simple.py
+++ server/src/buildyourbot/agents/simple.py
@@ -1,15 +1,41 @@
-from langchain_core.prompts import ChatPromptTemplate
+from functools import cache
+
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage, BaseMessage
+from langchain_core.runnables import RunnableConfig
 
 from src.buildyourbot.models import SimpleNode
+from src.buildyourbot.state import State
 
 class SimpleAgent:
-    def __init__(self, node: SimpleNode):
+    def __init__(self, node: SimpleNode, llm):
         self.__id = node['id']
         self.__name = node['name']
         self.__persona = node['persona']
         self.__dos = node['dos']
         self.__donts = node['donts']
         self.__examples = node['examples']
+        self.__llm = llm
+        self.__prompt = ChatPromptTemplate.from_messages([
+            SystemMessage(
+                f"""You are {self.__persona}
+                    Your jobs are to:
+                    {self.__dos}
+                    Make sure you follow these:
+                    {self.__donts}
+                    Here are some response examples for you:
+                    {self.__examples}
+                """
+            ),
+            MessagesPlaceholder("messages")    
+        ])
+        
+        self.__agent = self.__prompt | self.__llm
     
-    def create(self):
-        self.prompt
+    @cache
+    def node(self, state: State, config: RunnableConfig):
+        result = self.__agent.a_invoke(state, config)
+        result.name = self.__name
+        return {
+            "messages": [result]
+        }
