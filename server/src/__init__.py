c4975c55d4ab09990fbdeeef8f82a6cf907cddc1
Chinmay Pandya <chinmaypandya043@gmail.com>
2024-10-17 17:11:46 +0530

    feat(cors): cors in app

--- server/src/__init__.py
+++ server/src/__init__.py
@@ -2,9 +2,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .api import router as app_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
 
 app.include_router(app_router)
