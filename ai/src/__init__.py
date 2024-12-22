e8a33025dedac584daf8c65c586325603b8c3a95
Chinmay Pandya <chinmaypandya043@gmail.com>
2024-12-22 21:04:31 +0530

    fix(ai): cors method allow all

--- ai/src/__init__.py
+++ ai/src/__init__.py
@@ -12,7 +12,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
